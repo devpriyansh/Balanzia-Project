180d86cc9f671c2e64cec9f360944f35c590233b
devpriyansh <priyanshsahu853@gmail.com>
2025-07-20 17:13:20 +0530

    Changes server address to render domain

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://balanzia-backend.onrender.com/api",
   withCredentials: true, //to send cookies with request
 });
 
